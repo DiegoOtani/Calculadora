c511e8946cb216e5c9d92410e42ed07fd3fe3755
Diego Otani <142742827+DiegoOtani@users.noreply.github.com>
2024-03-06 10:40:06 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+class Calculadora {
+    constructor() {
+        this.display = document.querySelector('.display');
+
+        this.inicia = () => {
+            this.capturaCliques();
+            this.capturaEnter();
+        };
+
+        this.capturaCliques = () => {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+                if (el.classList.contains('btn-clear')) this.clear();
+                if (el.classList.contains('btn-del')) this.del();
+                if (el.classList.contains('btn-eq')) this.realizaConta();
+
+                this.display.focus();
+            });
+        };
+
+        this.capturaEnter = () => {
+            document.addEventListener('keypress', e => {
+                if (e.keyCode !== 13) return;
+                this.realizaConta();
+            });
+        };
+
+        this.addNumDisplay = el => this.display.value += el.innerText;
+
+        this.clear = () => this.display.value = '';
+
+        this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+        this.realizaConta = () => {
+            try {
+                const conta = eval(this.display.value);
+
+                if (!conta) {
+                    alert('Conta inválida');
+                    this.display.value = '';
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch (e) {
+                alert("Conta inválida");
+                this.display.value = '';
+                return;
+            }
+        };
+    }
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
